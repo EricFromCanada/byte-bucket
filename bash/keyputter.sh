96eae62166c81190879b7c35c9ae01b6d44c74c2
EricFromCanada <eric3knibbe@gmail.com>
2012-10-31 13:59:08 -0400

    adding keyputter

---
+++ bash/keyputter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Installs the current user's DSA key on all servers listed in the heredoc. 
+# If the key requires a passphrase, be sure to set up ssh-agent as well.
+# A key can be generated with: 
+# 	ssh-keygen -t dsa -f ~/.ssh/id_dsa -C "user@server.com"
+
+while read HOST
+do
+	echo "Installing on ${HOST}..."
+	cat ~/.ssh/id_dsa.pub | ssh root@${HOST} 'test ! -d "/root/.ssh" && mkdir /root/.ssh; cat - >> ~/.ssh/authorized_keys'
+done <<EOF
+server.com
