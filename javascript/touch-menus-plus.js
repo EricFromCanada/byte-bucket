ec3c71f4b6461b4c5b4a19db474cf0dfef339c03
Eric Knibbe <3324775+EricFromCanada@users.noreply.github.com>
2013-01-27 22:55:15 -0500

    new bash script, updated JS

--- javascript/touch-menus-plus.js
+++ javascript/touch-menus-plus.js
@@ -9,45 +9,48 @@
 // by a <div> containing the submenu.
 // This script must be loaded after the menu markup.
 
-// Change this to your menu's id value
-menuId = 'nav';
+// Change this to your menu's "id" value
+var menuId = 'nav';
 
-if(RegExp(" Mobile\\b").test(navigator.userAgent) && RegExp(" AppleWebKit/").test(navigator.userAgent)) {
+function handle_touchstart(item) {
+	return function () {
+		var itemAnchor = item.getElementsByTagName('a')[0],
+				itemSub = item.getElementsByTagName('div')[0],
+				currentSubmenuStyle = window.getComputedStyle(itemSub, '');
+		if ((currentSubmenuStyle.getPropertyValue('display') !== itemSub.defaultDisplay) ||
+				(currentSubmenuStyle.getPropertyValue('left') !== itemSub.defaultLeft) ||
+				(currentSubmenuStyle.getPropertyValue('overflow') !== itemSub.defaultOverflow) ||
+				(currentSubmenuStyle.getPropertyValue('right') !== itemSub.defaultRight) ||
+				(currentSubmenuStyle.getPropertyValue('visibility') !== itemSub.defaultVisibility)) {
+			itemAnchor.href = itemAnchor.getAttribute('type');
+		}
+	};
+}
+
+if (navigator.userAgent.match(/iPhone|iPod|iPad/i)) {
 	var menuItems = document.getElementById(menuId).getElementsByTagName('li');
-	for (var item = menuItems[0]; item != null; item = item.nextSibling) {
+	var item = null;
+	for (item = menuItems[0]; item !== null; item = item.nextSibling) {
 		if (item.nodeName === "LI") {
-			var itemChildren = item.childNodes;
-			var itemAnchors = new Array();
-			var itemSubs = new Array();
-			for (var itemChild = itemChildren[0]; itemChild != null; itemChild = itemChild.nextSibling) {
+			var itemChild, itemAnchor, itemSub = null;
+			for (itemChild = item.childNodes[0]; itemChild !== null; itemChild = itemChild.nextSibling) {
 				if (itemChild.nodeName === "A") {
-					itemAnchors.push(itemChild);
+					itemAnchor = itemChild;
 				} else if (itemChild.nodeName === "DIV") {
-					itemSubs.push(itemChild);
+					itemSub = itemChild;
 				}
-			}
-			if (itemAnchors.length > 0 && itemSubs.length > 0) {
-				itemAnchors[0].type = itemAnchors[0].getAttribute('href');
-				itemAnchors[0].href = '#';
-				var defaultSubmenuStyle = getComputedStyle(itemSubs[0], '');
-				itemSubs[0].defaultDisplay = defaultSubmenuStyle.getPropertyValue('display');
-				itemSubs[0].defaultLeft = defaultSubmenuStyle.getPropertyValue('left');
-				itemSubs[0].defaultOverflow = defaultSubmenuStyle.getPropertyValue('overflow');
-				itemSubs[0].defaultRight = defaultSubmenuStyle.getPropertyValue('right');
-				itemSubs[0].defaultVisibility = defaultSubmenuStyle.getPropertyValue('visibility');
-				item.ontouchstart = function(){
-					var itemAnchor = this.getElementsByTagName('a')[0];
-					var itemSub = this.getElementsByTagName('div')[0];
-					var currentSubmenuStyle = getComputedStyle(itemSub, '');
-					if ((currentSubmenuStyle.getPropertyValue('display') !== itemSub.defaultDisplay) ||
-							(currentSubmenuStyle.getPropertyValue('left') !== itemSub.defaultLeft) ||
-							(currentSubmenuStyle.getPropertyValue('overflow') !== itemSub.defaultOverflow) ||
-							(currentSubmenuStyle.getPropertyValue('right') !== itemSub.defaultRight) ||
-							(currentSubmenuStyle.getPropertyValue('visibility') !== itemSub.defaultVisibility)) {
-						itemAnchor.href = itemAnchor.getAttribute('type');
-						itemAnchors[0].type = '';
-					}
-				};
+				if (itemAnchor !== null && itemSub !== null) {
+					var defaultSubmenuStyle = window.getComputedStyle(itemSub, '');
+					itemAnchor.type = itemAnchor.getAttribute('href');
+					itemAnchor.href = '#';
+					itemSub.defaultDisplay = defaultSubmenuStyle.getPropertyValue('display');
+					itemSub.defaultLeft = defaultSubmenuStyle.getPropertyValue('left');
+					itemSub.defaultOverflow = defaultSubmenuStyle.getPropertyValue('overflow');
+					itemSub.defaultRight = defaultSubmenuStyle.getPropertyValue('right');
+					itemSub.defaultVisibility = defaultSubmenuStyle.getPropertyValue('visibility');
+					item.ontouchstart = handle_touchstart(item);
+					break;
+				}
 			}
 		}
 	}
