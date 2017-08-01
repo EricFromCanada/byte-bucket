a144eec9b946a8d4e12cb6465fe9cb9b8e34a636
Eric Knibbe <3324775+EricFromCanada@users.noreply.github.com>
2017-08-01 13:18:22 -0400

    keyputter: prefer RSA, allow username argument, fix ~/.ssh SELinux context

--- bash/keyputter.sh
+++ bash/keyputter.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Installs the current user's DSA key on all servers listed in the heredoc. 
+# Installs the current user's RSA key on all servers listed in the heredoc. 
+# The first argument will be used as the username, defaulting to root if none provided.
 # If the key requires a passphrase, be sure to set up ssh-agent as well.
 # A key can be generated with: 
-# 	ssh-keygen -t dsa -f ~/.ssh/id_dsa -C "user@server.com"
+# 	ssh-keygen -t rsa -f ~/.ssh/id_rsa -C "user@server.com"
+
+USER=${1:-root}
 
 while read HOST
 do
 	echo "Installing on ${HOST}..."
-	cat ~/.ssh/id_dsa.pub | ssh root@${HOST} 'test ! -d "/root/.ssh" && mkdir -p /root/.ssh; cat - >> ~/.ssh/authorized_keys'
+	cat ~/.ssh/id_rsa.pub | ssh ${USER}@${HOST} 'test ! -d "~/.ssh" && mkdir -p ~/.ssh; cat - >> ~/.ssh/authorized_keys; type restorecon >/dev/null 2>&1 && restorecon -R ~/.ssh'
 done <<EOF
 server.com
